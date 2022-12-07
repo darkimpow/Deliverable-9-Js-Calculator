0205a894ab2f830497c8651688847f5ea2084bc5
darkimpow <darkimpowell@gmail.com>
2022-12-07 15:26:25 -0500

    fixed app.js file with functions

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-<script>
-// This function solves the values
-function solve(){ let y= eval document.getElementById('screen').value
-document.getElementById('screen').value=y
+function solve() {
+    let y = eval(document.getElementById('screen').value);
+    document.getElementById('screen').value=y;
+    
 }
 
 // This function clear all the values
-function clr(){
-            document.getElementById('screen').value='';
-            }
+        function clr() {
+        document.getElementById('screen').value="";
+         }
 
 //This function shows the screen display
-function dis(value){
-    document.getElementById('screen').value=val
+function dis(value) {
+    document.getElementById('screen').value=val;
 }
-
-</script>
