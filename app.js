3f142ae1ffeb88a70477a6bb65ad1b9c5578875b
darkimpow <darkimpowell@gmail.com>
2022-12-07 11:01:43 -0500

    added functions to js.app file

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 <script>
-// This function clear all the values
+// This function solves the values
 function clearScreen() {
 document.getElementById("result").value = "";
 }
 
-// This function display values
-function display(value) {
-document.getElementById("result").value += value;
+// This function clear all the values
+function clearScreen() {
+document.getElementById("result").value = "";
 }
-
-// This function evaluates the expression and returns result
-function calculate() {
-var p = document.getElementById("result").value;
-var q = eval(p);
-document.getElementById("result").value = q;
 
 </script>
